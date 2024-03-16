0c85218c546066af1606ce3cb77f86badb889479
dfghjkcvbnmnbvcx <163094726+njkih@users.noreply.github.com>
2024-03-17 00:44:08 +0700

    init

---
+++ index.js
@@ -0,0 +1,11 @@
+// index.js
+
+function convertToUppercase(str) {
+    return str.toUpperCase();
+  }
+  
+  const text = 'hello world';
+  const uppercaseText = convertToUppercase(text);
+  console.log(`Original text: ${text}`);
+  console.log(`Uppercase text: ${uppercaseText}`);
+  
